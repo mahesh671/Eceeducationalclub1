9eabb9712e805d1f43724e88505beafd4a2f1d08
mahesh671 <maheshkumarpadmanabhuni498@gmail.com>
2020-10-23 19:09:06 +0530

    Added Notifications by Firebase Cloud Messaging

--- app/src/main/java/com/example/eceeducationalclub/MainActivity.java
+++ app/src/main/java/com/example/eceeducationalclub/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
 Button b1,b2,b3;
@@ -41,4 +44,5 @@ Button b1,b2,b3;
             }
         });
     }
+
 }
